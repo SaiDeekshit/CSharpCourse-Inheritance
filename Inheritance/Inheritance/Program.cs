c97b0e403242b87663ca7154b741e3bae973bb3a
Sai Deekshith <msaideekshith@gmail.com>
2020-03-27 07:23:00 +0530

    Inheritance

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Programmer p1 = new Programmer();
+
+            Console.WriteLine("Salary: " + p1.salary);
+            Console.WriteLine("Bonus: " + p1.bonus);
+        }
+    }
+    //Parent Class
+    public class Employee
+    {
+        public float salary = 40000;
+    }
+    //Child Class
+    public class Programmer : Employee
+    {
+        public float bonus = 10000;
+    }
+}
